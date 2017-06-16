f8fbc3387553084251ce6d4d8eaa2557571e0d99
lscarlon <lscarlon@libero.it>
2017-06-16 15:39:55 +0200

    Prima versione vuota funzionante

--- AdminPanel/Controllers/HomeController.cs
+++ AdminPanel/Controllers/HomeController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AdminPanel.Attributes;
 
 namespace AdminPanel.Controllers
 {
+    [DisplayOrder(0)]
+    [DisplayImage("fa fa-dashboard")]
+    [TreeView("i", "fa fa-angle-left pull-right", "")]
     public class HomeController : Controller
     {
+        [DisplayActionMenu]
+        [DisplayImage("fa fa-circle-o")]
+        [ScriptAfterPartialView("")]
         public IActionResult Default()
         {
             return View();
